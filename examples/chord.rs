959b832de6ca29652617ede9df4681e4d1beaeb2
Kai Schmidt <kaikaliischmidt@gmail.com>
2022-03-29 13:31:19 -0700

    improve chord example

--- examples/chord.rs
+++ examples/chord.rs
@@ -1,21 +1,23 @@
 use std::time::Duration;
 
-use hodaun::{
-    gen::{Sine, Wave},
-    DeviceMixer, Mixer, Mono, Source,
-};
+use hodaun::{gen::SineWave, DeviceMixer, Mixer, Mono, Source};
 
 const SAMPLE_RATE: f32 = 44100.0;
 
 fn main() {
+    // Initializer the output and a chord mixer
     let mut output = DeviceMixer::<Mono>::with_default_device().unwrap();
-    let (mixer, mixer_source) = Mixer::new();
+    let (chord, chord_source) = Mixer::new();
 
-    mixer.add(Wave::<Sine>::new(220.0, SAMPLE_RATE).amplify(0.5));
-    mixer.add(Wave::<Sine>::new(220.0 * 2f32.powf(4.0 / 12.0), SAMPLE_RATE).amplify(0.5));
-    mixer.add(Wave::<Sine>::new(220.0 * 2f32.powf(7.0 / 12.0), SAMPLE_RATE).amplify(0.5));
+    // Add notes to the chord
+    let base = 220.0;
+    chord.add(SineWave::new(base, SAMPLE_RATE).amplify(0.5));
+    chord.add(SineWave::new(base * 2f32.powf(4.0 / 12.0), SAMPLE_RATE).amplify(0.5));
+    chord.add(SineWave::new(base * 2f32.powf(7.0 / 12.0), SAMPLE_RATE).amplify(0.5));
 
-    output.add(mixer_source.take(Duration::from_secs(1)));
+    // Add the chord to the output, only playing for 3 seconds
+    output.add(chord_source.take(Duration::from_secs(3)));
 
+    // Play
     output.blocking_play().unwrap();
 }
